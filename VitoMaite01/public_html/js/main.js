9472c890992d26ba8f17873c5db1fcacdcb047cd
bsagarzazu <sagartertzio25@gmail.com>
2024-11-19 10:47:05 +0100

    Cambios para getAll() hobbies

--- VitoMaite01/public_html/js/main.js
+++ VitoMaite01/public_html/js/main.js
@@ -66,3 +66,24 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "resultados.html"; // Página de resultados
     });
 });
+
+function getHobbies() {
+    let request = window.indexedDB.open("vitomaite01", 1);
+
+    let db;
+    request.onsucces = (event) => {
+        db = event.target.result;
+    };
+
+    const transaction = db.transaction(['hobbies'], 'readonly');
+    const objectStore = transaction.objectStore('hobbies');
+
+    request = objectStore.getAll();
+    request.onsucces = function (event) {
+        const hobbies = event.target.result;
+        return hobbies;
+    };
+    request.onerror = (event) => {
+        console.error("An error occurred during database opening: ${event.target.error?.message}");
+    };
+}
