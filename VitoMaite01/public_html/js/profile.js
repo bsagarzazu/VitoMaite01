8011696bfc6f03862dd64007a6b0a0af0eb98edb
yuyuan jiang <yuyuanjiang0425@gmail.com>
2024-11-19 11:06:57 +0100

    modiPerfil2

--- VitoMaite01/public_html/js/profile.js
+++ VitoMaite01/public_html/js/profile.js
@@ -12,8 +12,16 @@ document.addEventListener("", function() {
     pGenero.value += usu.gender;
     const pFoto = document.getElementById("profile-photo");
     pFoto.value += usu.imagen;
-    
-    //const pAficion = document.getElementById("profile-hobbies");
-    //pAficion.value += userHobby.;
-    
+    /*
+    const request = window.indexedDB.open("vitomaite01",1);
+    request.onsuccess = (event) =>{
+        const db = event.target.result;
+        const trans = db.transaction(["hobbies", "userHobby"],"readonly");
+        const tableHobbies = trans.objectStore("hobbies");
+        const tableUserHobbies = trans.objectStore("userHobby");
+        
+        tableUserHobbies
+    };
+     * 
+     */
 });
