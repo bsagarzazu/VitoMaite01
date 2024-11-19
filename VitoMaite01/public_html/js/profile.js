fea5e364a099f41b1fde38a3ffe59fe23d0bc31d
yuyuan jiang <yuyuanjiang0425@gmail.com>
2024-11-19 15:11:21 +0100

    modiPerfil1

--- VitoMaite01/public_html/js/profile.js
+++ VitoMaite01/public_html/js/profile.js
@@ -12,6 +12,7 @@ document.addEventListener("", function() {
     pGenero.value += usu.gender;
     const pFoto = document.getElementById("profile-photo");
     pFoto.value += usu.imagen;
+    
     /*
     const request = window.indexedDB.open("vitomaite01",1);
     request.onsuccess = (event) =>{
