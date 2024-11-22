4195b9de9b2700ae70222858478b6bda120d5081
aritzfloresmonje <>
2024-11-22 20:50:04 +0100

    Ver likes y matches funcionando

--- VitoMaite01/public_html/js/headerFooter.js
+++ VitoMaite01/public_html/js/headerFooter.js
@@ -41,6 +41,162 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     likesBtn.addEventListener("click", function () {
-        window.location.href = "likes.html";
+        displayLikesWindow();
     });
 });
+
+function displayLikesWindow() {
+    const likesModal = document.getElementById("likes-profile-modal");
+        
+    const closeBtn = document.getElementById("likes-close-btn");
+    closeBtn.addEventListener("click", () => {
+        likesModal.style.display = "none";
+    });
+    
+    const likesDiv = document.getElementById("likes");
+    likesDiv.innerHTML = "";
+    displayData(likesDiv);
+
+    likesModal.style.display = "flex";    
+}
+
+function displayData(likesDiv) {
+    const loggedUser = JSON.parse(sessionStorage.getItem("userLoggedIn"));
+    
+    getLikes(loggedUser.email)
+        .then((likes) => {
+            // TODO: meterlo en el siguiente .then
+            return getUsers(likes);
+        })
+        .then((users) => {
+            const likedBy = users.likes;
+            const matches = users.matches;
+            
+            // Primero se muestran los matches
+            matches.forEach((match) => {
+                const matchDiv = document.createElement("div");
+                matchDiv.className = "likes-profile";
+                matchDiv.innerHTML = `
+                    <p><span class="name">${match.nick}</span>  ${match.age}  <img class="match-img" src="img/heart_match.png"></p>
+                `;
+                likesDiv.appendChild(matchDiv);
+            });
+            
+            likedBy.forEach((like) => {
+                const likeDiv = document.createElement("div");
+                likeDiv.className = "likes-profile";
+                likeDiv.innerHTML = `
+                    <p><span class="name">${like.nick}</span>  ${like.age}</p>
+                `;
+                likesDiv.appendChild(likeDiv);
+            });
+        });
+}
+
+function getLikes(userEmail) {
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open("vitomaite01", 1);
+        
+        request.onerror = (event) => {
+            console.log(`Error opening database: ${event.target.error?.message}`);
+            reject(event.target.error);
+        };
+        
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            const likedBy = {likes: [], matches: []};
+            
+            const transaction = db.transaction(["likes"], "readonly");
+            const objStore = transaction.objectStore("likes");
+            const index = objStore.index("byEmail1");
+            
+            const likedUsersPromise = new Promise((resolve, reject) => {
+                const likedUsers = index.get(userEmail);
+                likedUsers.onsuccess = (event) => {
+                    resolve(event.target.result);
+                };
+            });
+            
+            const getAllPromise = new Promise((resolve, reject) => {
+                const getAllRequest = objStore.getAll();
+                getAllRequest.onsuccess = (event) => {
+                    resolve(event.target.result);
+                };
+            });
+            
+            Promise.all([likedUsersPromise, getAllPromise])
+                .then((values) => {
+                    const likedUsers = values[0];
+                    const allValues = values[1];
+                    // Para cada valor en allValues, mirar si email2 coincide con parámetro
+                    // Sí -> mirar si email1 está en likedUsers
+                    allValues.forEach((value) => {
+                        // Cada valor: (email1, email2)
+                        if(value.email2 === userEmail) {
+                            // Comprobar si está en likedUsers
+                            if(search(value.email1, likedUsers)) {
+                                likedBy.matches.push(value.email1);
+                            }
+                            else {
+                                likedBy.likes.push(value.email1);
+                            }
+                        }
+                    });
+                    resolve(likedBy);
+                });
+        };
+    });
+}
+
+function search(email, list) {
+    if(list.length === 1 || !Array.isArray(list)) {
+        return email === list.email2;
+    }
+    
+    for (let i = 0; i < list.length; i++) {
+        if(email === list[i].email2) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getUsers(likedBy) {
+    const users = {likes: [], matches: []};
+    
+    // Esperar a que se cumplan las promesas de cada usuario
+    const likesPromises = likedBy.likes.map((email) => getUser(email));
+    const matchesPromises = likedBy.matches.map((email) => getUser(email));
+    
+    return Promise.all([Promise.all(likesPromises), Promise.all(matchesPromises)])
+        .then(([likes, matches]) => {
+            users.likes = likes;
+            users.matches = matches;
+            return users;
+        });
+}
+
+function getUser(email) {
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open("vitomaite01", 1);
+        
+        request.onerror = (event) => {
+            console.log(`Error opening database: ${event.target.error?.message}`);
+            reject(event.target.error);
+        };
+        
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            
+            const index = db
+                .transaction(["users"], "readonly")
+                .objectStore("users")
+                .index("byEmail");
+        
+            const getRequest = index.get(email);
+            getRequest.onsuccess = (event) => {
+                resolve(event.target.result);
+            };
+        };
+    });
+}
