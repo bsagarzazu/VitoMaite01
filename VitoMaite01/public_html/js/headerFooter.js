5b41327802adfcdbeb9b1a6238559af0d5b3caae
bsagarzazu <sagartertzio25@gmail.com>
2024-11-25 10:30:00 +0100

    Botón edit fuera

--- VitoMaite01/public_html/js/headerFooter.js
+++ VitoMaite01/public_html/js/headerFooter.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const likesBtn = document.getElementById("likes-btn");
     const logoutBtn = document.getElementById("logout-btn");
     const profileBtn = document.getElementById("view-profile-btn");
-    const editBtn = document.getElementById("edit-profile-btn");
     const userInfo = document.getElementById("user-info");
     const userLogged = document.getElementById("user-logged");
     const userGuest = document.getElementById("user-guest");
@@ -41,10 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     profileBtn.addEventListener("click", function () {
         window.location.href = "profile.html";
-    });
-    
-    editBtn.addEventListener("click", function () {
-        window.location.href = "edit.html";
     });
 
     mapBtn.addEventListener("click", function () {
